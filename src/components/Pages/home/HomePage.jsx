35032e5ff237015830c66700ec100fad8478e9bd
varsha-2530 <vb8607518@gmaail.com>
2026-06-18 16:46:49 +0530

    code push

--- src/components/Pages/home/HomePage.jsx
+++ src/components/Pages/home/HomePage.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
   return (
     <div>
       <Hero />
-      <OurPortfolio />
+      {/* <OurPortfolio /> */}
       <AboutSection />
       <WhyChooseUs />
       <ServicesSection />
